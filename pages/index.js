b966c0feddfec39ae1d90397f6c3ec25258a6259
GriotBot <chat@griotbot.com>
2025-05-13 09:35:34 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
-
+// In /pages/index.js
+export default function Home() {
+  return (
+    <>
+      <Head>
+        {/* head content */}
+      </Head>
+      
+      <div id="header" role="banner">
+        <button id="toggleSidebar">☰</button>
+        <div className="logo-container">
+          <span className="logo-icon">🌿</span>
+          <span>GriotBot</span>
+        </div>
+        <button id="themeToggle"></button>
+      </div>
+      
+      {/* Rest of your components */}
+    </>
+  );
+}
