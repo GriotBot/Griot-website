7e0e054f7e8bef7e8c02ffdcab04062881d2ad4e
GriotBot <chat@griotbot.com>
2025-06-05 23:43:47 -0400

    Update StandardLayout.js

--- components/layout/StandardLayout.js
+++ components/layout/StandardLayout.js
@@ -6,8 +6,35 @@ import { useRouter } from 'next/router';
 import { Menu, Home, User, Sun, Moon } from 'react-feather';
 import MessageCirclePlus from '../icons/MessageCirclePlus';
 import EnhancedSidebar from './EnhancedSidebar';
-import { PROVERBS, THEME_STORAGE_KEY, CONTACT_INFO } from '../../lib/constants';
 import styles from '../../styles/Footer.module.css';
+
+// Import constants - adjust path based on where this file is used
+const PROVERBS = [
+  "Wisdom is like a baobab tree; no one individual can embrace it. — African Proverb",
+  "Until the lion learns to write, every story will glorify the hunter. — African Proverb",
+  "We are the drums, we are the dance. — Afro-Caribbean Proverb",
+  "A tree cannot stand without its roots. — Jamaican Proverb",
+  "Unity is strength, division is weakness. — Swahili Proverb",
+  "Knowledge is like a garden; if it is not cultivated, it cannot be harvested. — West African Proverb",
+  "Truth is like a drum, it can be heard from afar. — Kenyan Proverb",
+  "A bird will always use another bird's feathers to feather its nest. — Ashanti Proverb",
+  "You must act as if it is impossible to fail. — Yoruba Wisdom",
+  "The child who is not embraced by the village will burn it down to feel its warmth. — West African Proverb",
+  "However long the night, the dawn will break. — African Proverb",
+  "If you want to go fast, go alone. If you want to go far, go together. — African Proverb",
+  "It takes a village to raise a child. — African Proverb",
+  "The fool speaks, the wise listen. — Ethiopian Proverb",
+  "When the music changes, so does the dance. — Haitian Proverb"
+];
+
+const THEME_STORAGE_KEY = 'griotbot-theme';
+
+const CONTACT_INFO = {
+  email: 'chat@griotbot.com',
+  instagram: 'https://www.instagram.com/griotbot',
+  twitter: 'https://twitter.com/griotbot',
+  linkedin: 'https://www.linkedin.com/company/griotbot'
+};
 
 export default function StandardLayout({ 
   children, 
